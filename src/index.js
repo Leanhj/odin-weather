d429ebe540ba2a720327968d72c7fd8eede4cab5
leanhj <leanhiga12@gmail.com>
2025-06-01 21:18:43 -0300

    Fetch input value from search bar

--- src/index.js
+++ src/index.js
@@ -11,14 +11,17 @@ const response = await fetchWeatherData("london");
 const forecast = createForecastWith(response);
 const futureForecast = createFutureForecastWith(response);
 const resolvedAddress = response.resolvedAddress;
+const divAddress = document.querySelector("#address");
+divAddress.textContent = resolvedAddress;
 
 let inputValue = "london";
 const inputSearchbar = document.querySelector("#search-input");
 const buttonSearchbar = document.querySelector("#search-button");
-buttonSearchbar.addEventListener("click", (e) => {
+buttonSearchbar.addEventListener("click", async (e) => {
     e.preventDefault;
     inputValue = inputSearchbar.value;
+    const response = await fetchWeatherData(inputValue);
+    const forecast = createForecastWith(response);
+    const futureForecast = createFutureForecastWith(response);
+    divAddress.textContent = response.resolvedAddress;
 });
-
-const divAddress = document.querySelector("#address");
-divAddress.textContent = resolvedAddress;
